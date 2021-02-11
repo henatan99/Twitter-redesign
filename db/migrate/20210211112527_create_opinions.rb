86336f957947750ebfcd76fc6acb1585314ecbc3
Henok <henatan99@gmail.com>
2021-02-11 14:31:57 +0300

    - adds user scaffold with user table columns of username, fullname, photo:string and coverimage:string

--- db/migrate/20210211112527_create_opinions.rb
+++ db/migrate/20210211112527_create_opinions.rb
@@ -1,7 +1,7 @@
 class CreateOpinions < ActiveRecord::Migration[6.1]
   def change
     create_table :opinions do |t|
-      t.string :text
+      t.text :text
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_161536) do
+ActiveRecord::Schema.define(version: 2021_02_11_113040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "opinions", force: :cascade do |t|
-    t.string "text"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "fullname"
+    t.string "photo"
+    t.string "coverimage"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
