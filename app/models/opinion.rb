091cecdf2de5d5caa1de7d63d424bf238684ad6b
Henok <henatan99@gmail.com>
2021-02-11 20:37:25 +0300

    - adds validations to models - adds specs files for models - tests models with validation and association examples

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,3 +1,6 @@
 class Opinion < ApplicationRecord
-    belongs_to :author
+    belongs_to :author, foreign_key: :author_id, class_name: 'User'
+
+    validates :author, presence: true, uniqueness: true
+    validates :text, presence: true, length: { minimum: 1, maximum: 100 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,45 @@
 require "./rails_helper"
 
-RSpec.describe User, type: :model do
-    let(:user) { User.create(username: 'elisha', fullname: 'Elish Kapong') }
-    it 'is valid with valid attributes' do
-      expect(user).to be_valid
+RSpec.describe User do
+    let(:subject) do
+      described_class.new(
+        username: 'myusername',
+        fullname: 'myuser fullname'        
+      )
     end
   
-    it 'is not valid without a name' do
-      user.name = nil
-      expect(user).to_not be_valid
-    end
+    describe 'validations' do
+      it { is_expected.to validate_presence_of(:username) }
+      it { is_expected.to validate_presence_of(:fullname) }      
   
-    context 'validations' do
-      it 'presence' do
-        user.email = nil
-        expect(user).to_not be_valid
+      it 'is valid with valid attribute' do
+        expect(subject).to be_valid
       end
   
-      it 'uniqueness' do
-        User.create(name: 'elisha', email: 'elisha@gmail.com', encrypted_password: 'elishapassword')
-        user2 = User.create(name: 'elisha', email: 'elisha@gmail.com', encrypted_password: 'elishapassword')
-        expect(user2).to_not be_valid
-      end 
-      
+      it 'The username of the user should exist' do
+        subject.username = ''
+        expect(subject).not_to be_valid
+      end
+  
+      it 'The name of the user should not be longer than 20 characters' do
+        subject.username = 'weiruwieruiowueriuioweuriuwioeurwioeriouiowerui'
+        expect(subject).not_to be_valid
+      end
+  
+      it 'The full name should exist' do
+        subject.fullname = ''
+        expect(subject).not_to be_valid
+      end
+  
+      it 'The fullname should not be longer than 30 characters' do
+        subject.fullname = 'kwehjkwehfkjewhfewhfkdhfkdhfkdhfkdjfkldjfkljflkwjflwdjflkdjfkldjfklsdf'
+        expect(subject).not_to be_valid
+      end
     end
-  end
+  
+    describe 'Associations', type: :model do
+      it { is_expected.to have_many(:opinions) }
+      it { is_expected.to have_many(:followings) }
+      it { is_expected.to have_many(:followers) }      
+    end
+end
