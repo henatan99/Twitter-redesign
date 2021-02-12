57e7a7b78c62e3c7799a99fa607168dff09851f5
Henok <henatan99@gmail.com>
2021-02-12 14:09:04 +0300

    adds static contents to the layout/application.html.erb and adds scss stylings for different sections which later will move to different view pages except the nav section

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_many :opinions, 
         foreign_key: :author_id,
-        dependent: :destroy,
-        inverse_of: 'author'    
+        dependent: :destroy
+        # inverse_of: 'author'    
     has_many :followings,
         foreign_key: :follower_id,        
         dependent: :destroy,
