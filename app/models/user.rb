c6da2fad3e4cca3f5dc692dad29fd9835592db34
Henok <henatan99@gmail.com>
2021-02-15 02:53:34 +0300

    adds more stylings on the opionion index and user show pages and fixes bugs on following-user models associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
     has_many :opinions, 
         foreign_key: :author_id,
-        dependent: :destroy,
-        inverse_of: 'author'    
-    has_many :followings,
-        foreign_key: :follower_id,        
-        dependent: :destroy,
-        inverse_of: 'follower'
-    has_many :followers, through: :followings, foreign_key: :followed_id    
-
+        dependent: :destroy
+            
+    has_many :followings, class_name: 'Following', foreign_key: 'follower_id'
+    has_many :inverse_followings, class_name: 'Following', foreign_key: 'followed_id'
+        
+    has_many :followeds, through: :followings, foreign_key: 'followed_id'
+    has_many :followers, through: :inverse_followings, foreign_key: 'follower_id'
 
     validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
     validates :fullname, presence: true, length: { maximum: 30 }
