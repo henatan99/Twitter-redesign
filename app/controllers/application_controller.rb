db4cb991ca31f23ececdaf0d318876006d598095
Henok Mekonnen Mossissa <henatan99@yahoo.com>
2021-02-18 00:02:48 +0300

    Revert "This pull request is to merge the feature-1 branch to development branch of the 'twitter redesign' app building for Microverse's ruby on rails capstone project"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,2 @@
 class ApplicationController < ActionController::Base
-  private
-
-  def require_login
-    return if current_user
-
-    redirect_to new_session_path, alert: 'Sign up or Log in!'
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  helper_method :current_user
 end
